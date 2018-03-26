a8234ebc8c4cc1dd7285b545b160fd68cb45f51f
chieftaha <chieftaha@gmail.com>
2018-03-26 09:34:49 +0430

    Laravel Installation

---
+++ config/apollo.php
@@ -0,0 +1,11 @@
+<?php
+return [
+     'book' => [
+          'name' => "Laravel, Zero to Sixty" ,
+          'author' => "Taha Kamkar" ,
+     ],
+     'measuring_unit' => [
+          'length' => "m" ,
+          'weight' => "kg" ,
+     ],
+];
